b77c21b2681474956264be76f37189e001f70bd8
Syed Owais Ahmed <46538428+SyedOwaisAhmed222@users.noreply.github.com>
2022-12-29 21:35:40 +0500

    Add files via upload

--- Backend/myfile.py
+++ Backend/myfile.py
@@ -5,16 +5,41 @@ Spyder Editor
 This is a temporary script file.
 """
 from flask import Flask
+from flask_restful import Resource , Api, reqparse, abort
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def index():
-    return "Town Water Management System"
+data= {
+    1: {"mykey": "myvalue", "description": "my first object"},
+    2: {"mykey": "myvalue", "description": "my second object"}
+    }
 
-@app.route("/reading")
-def readings():
-    reading= 69
-    return str(reading)
+data_post_args= reqparse.RequestParser()
+data_post_args.add_argument("mykey", type=str, help = "data is required", required=True)
+data_post_args.add_argument("description", type=str, help = "description is required", required=True)
+
+
+class myObjectsList(Resource):
+    def get(self):
+        return data
+
+class myObjects(Resource):
+    def get(self, data_id):
+        return data[data_id]
+    
+    def post(self,data_id):
+        args= data_post_args.parse_args()
+        if data_id in data:
+            abort(409, "data_id already taken")
+        data[data_id]= {"mykey": args["mykey"], "description": args["description"]}
+        return data[data_id]
+    
+    
+
+api.add_resource(myObjects, '/data/<int:data_id>')
+api.add_resource(myObjectsList, '/data')
+
 
 if __name__=="__main__":
     app.run(debug=True)
